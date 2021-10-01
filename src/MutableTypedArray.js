013cfc5c38c730200f57119a3876dd43ca5d7c62
UmamiAppearance <mail@umamiappearance.eu>
2021-10-01 15:39:31 +0200

    first steps

---
+++ src/MutableTypedArray.js
@@ -0,0 +1,54 @@
+const ArrayTypes = {
+    Int8Array: Int8Array,
+    Int16Array: Int16Array,
+    Int32Array: Int32Array,
+    Uint8Array: Uint8Array,
+    Uint8ClampedArray: Uint8ClampedArray,
+    Uint16Array: Uint16Array,
+    Uint32Array: Uint32Array,
+    Float32Array: Float32Array,
+    Float64Array: Float64Array,
+}
+
+class MutableTypedArray {
+    constructor() {
+        console.log("hello");
+    }
+
+    static getType(obj) {
+        return obj.constructor.name;
+    }
+    
+    static isTypedArray(obj) {
+        return obj.constructor.name in ArrayTypes;
+    }
+
+    static isTypeOf(obj, type) {
+        return obj.constructor.name === type;
+    }
+
+    static pushTo(obj, b) {
+        const newArray = new ArrayTypes[obj.constructor.name](obj.length + 1);
+        newArray.set(obj);
+        newArray[obj.length] = b;
+        return newArray;
+    }
+
+    static shiftTo(obj, b) {
+        const newArray = new ArrayTypes[obj.constructor.name](obj.length + 1);
+        newArray.set(obj, 1);
+        newArray[0] = b;
+        return newArray;
+    }
+
+    static concat(objA, objB) {
+        if (objA.constructor.name !== objB.constructor.name) {
+            throw new TypeError(`You are trying to concatenate two different types of arrays ('${objA.constructor.name}' and '${objB.constructor.name}')\nThis can only be done by converting them into the same type before.`);
+        }
+        const newArray = new ArrayTypes[objA.constructor.name](objA.length + objB.length);
+        newArray.set(objA);
+        newArray.set(objB, objA.length);
+        return newArray;
+    }
+
+}
