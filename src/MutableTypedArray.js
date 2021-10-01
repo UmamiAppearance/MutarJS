e309615ba575205c84e55f83240484e6521b2891
UmamiAppearance <mail@umamiappearance.eu>
2021-10-01 17:51:05 +0200

    kind of working

--- src/MutableTypedArray.js
+++ src/MutableTypedArray.js
@@ -1,18 +1,45 @@
 const ArrayTypes = {
     Int8Array: Int8Array,
-    Int16Array: Int16Array,
-    Int32Array: Int32Array,
     Uint8Array: Uint8Array,
     Uint8ClampedArray: Uint8ClampedArray,
+    Int16Array: Int16Array,
     Uint16Array: Uint16Array,
+    Int32Array: Int32Array,
     Uint32Array: Uint32Array,
     Float32Array: Float32Array,
-    Float64Array: Float64Array,
+    Float64Array: Float64Array
 }
 
 class MutableTypedArray {
-    constructor() {
-        console.log("hello");
+    constructor(input, type) {
+        const MTAObj = new Object();
+        if (ArrayBuffer.isView(input)) {
+            MTAObj.type = input.constructor.name;
+            MTAObj.typeConstructor = ArrayTypes[MTAObj.type];
+            MTAObj.array = input;
+            MTAObj.buffer = MTAObj.array.buffer;
+        } else {
+            let error = true;
+            if (type) {
+                if (typeof(type) === "function") type = type.name;
+                MTAObj.type = type;
+                if (!(MTAObj.type in ArrayTypes)) throw new TypeError(`Unknown type: ${MTAObj.type}`);
+                MTAObj.typeConstructor = ArrayTypes[type];
+                if (input instanceof ArrayBuffer || Array.isArray(input)) {
+                    MTAObj.array = new MTAObj.typeConstructor(input);
+                    MTAObj.buffer = MTAObj.array.buffer;
+                    error = false;
+                }
+            }
+            if (error) throw new TypeError("Input must be a TypedArray, an ArrayBuffer or an Array. For Array and ArrayBuffer the type needs to be specified as a second argument.");
+        }
+        MTAObj.view = new DataView(MTAObj.buffer);
+
+        // append static functions
+        
+
+
+        return MTAObj;
     }
 
     static getType(obj) {
