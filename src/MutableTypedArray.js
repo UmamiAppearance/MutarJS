f66c9b7226307c9eda359f757f83844e2bae7d78
UmamiAppearance <mail@umamiappearance.eu>
2021-10-03 00:03:47 +0200

    mostly done

--- src/MutableTypedArray.js
+++ src/MutableTypedArray.js
@@ -7,23 +7,31 @@ const ArrayTypes = {
     Int32Array: Int32Array,
     Uint32Array: Uint32Array,
     Float32Array: Float32Array,
-    Float64Array: Float64Array
+    Float64Array: Float64Array,
+    BigInt64Array: BigInt64Array,
+    BigUint64Array: BigUint64Array
 }
 
 class MutableTypedArray {
     constructor(input, type) {
+        
         const MTAObj = {
             set arrayContent(val) {
                 this.array = val;
                 this.buffer = val.buffer;
+                this.byteLength = val.byteLength;
+                this.byteOffset = val.byteOffset;
+                this.length = this.array.length;
                 this.view = new DataView(val.buffer);
             },
             type: null,
             typeConstructor: null,
         }
+
         if (typeof(input) === "string") {
             input = new TextEncoder().encode("string");
         }
+        
         if (ArrayBuffer.isView(input)) {
             MTAObj.arrayContent = input;
             MTAObj.type = input.constructor.name;
@@ -35,7 +43,7 @@ class MutableTypedArray {
                     type = type.name;
                 }
                 if (!(type in ArrayTypes)) {
-                    throw new TypeError(`Unknown type: ${MTAObj.type}`);
+                    throw new TypeError(`Unknown type: ${type}`);
                 }
                 MTAObj.type = type;
                 MTAObj.typeConstructor = ArrayTypes[type];
@@ -93,28 +101,56 @@ class MutableTypedArray {
         obj.concat = (arr) => {
             const array = MutableTypedArray.concat(obj.array, arr);
             return new MutableTypedArray(array);
-        },
+        };
+
+        obj.concatSet = (arr) => obj.arrayContent = obj.concat(arr).array;
 
         obj.push = (b) => {
-            obj.array = MutableTypedArray.pushTo(obj.array, b);
-            return obj.array.slice(-1)[0];
-        },
+            obj.arrayContent = MutableTypedArray.pushTo(obj.array, b);
+            return obj.array.at(-1);
+        };
 
         obj.pop = () => {
-            const popped = obj.array.slice(-1)[0];
+            const popped = obj.array.at(-1)
             obj.arrayContent = obj.array.slice(0, -1);
             return popped;
-        },
+        };
 
-        obj.shift = () => {
+        obj.shift = () => {array
             const shifted = obj.array[0];
             obj.arrayContent = obj.array.slice(1);
             return shifted;
-        },
+        };
 
         obj.unshift = (b) => {
-            obj.array = MutableTypedArray.unshiftTo(obj.array, b);
+            obj.arrayContent = MutableTypedArray.unshiftTo(obj.array, b);
             return obj.array[0];
-        }
+        };
+
+        // make build in methods accessible
+        obj.at = (...args) => obj.array.at(...args);
+        obj.copyWithin = (...args) => obj.array.copyWithin(...args);
+        obj.entries = (...args) => obj.array.entries(...args);
+        obj.every = (...args) => obj.array.every(...args);
+        obj.fill = (...args) => obj.array.fill(...args);
+        obj.filter = (...args) => obj.array.filter(...args);
+        obj.find = (...args) => obj.array.find(...args);
+        obj.findIndex = (...args) => obj.array.findIndex(...args);
+        obj.forEach = (...args) => obj.array.forEach(...args);
+        obj.includes = (...args) => obj.array.includes(...args);
+        obj.indexOf = (...args) => obj.array.indexOf(...args);
+        obj.join = (...args) => obj.array.join(...args);
+        obj.keys = (...args) => obj.array.keys(...args);
+        obj.lastIndexOf = (...args) => obj.array.lastIndexOf(...args);
+        obj.map = (...args) => obj.array.map(...args);
+        obj.reduce = (...args) => obj.array.reduce(...args);
+        obj.reduceRight = (...args) => obj.array.reduceRight(...args);
+        obj.reverse = (...args) => obj.array.reverse(...args);
+        obj.set = (...args) => obj.array.set(...args);
+        obj.slice = (...args) => obj.array.slice(...args);
+        obj.some = (...args) => obj.array.some(...args);
+        obj.sort = (...args) => obj.array.sort(...args);
+        obj.subarray = (...args) => obj.array.subarray(...args);
+        obj.values = (...args) => obj.array.values(...args);
     }
 }
