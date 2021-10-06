f789c19864c39267d3e25bf0ba2e36f258f670a7
UmamiAppearance <mail@umamiappearance.eu>
2021-10-06 10:55:54 +0200

    test progress

--- test/test.mjs
+++ test/test.mjs
@@ -11,10 +11,13 @@ const results = {
 function makeError(unit, subUnit, input, output, expected) {
     results.errors++;
     results.units[unit].errors++;
-    const IOpart = `Input: ${input}\nOutput: ${output}\nExpected: ${expected}`;
-    results.errorMessages[unit][`#${results.errors}`] = `${subUnit}: ${IOpart}`;
-
-    const message = `Error occurred while testing '${unit}' ('${subUnit}'):\n${IOpart}`;
+    results.errorMessages[unit][`#${results.errors}`] = {
+        "sub-unit": subUnit,
+        input: input,
+        output: output,
+        expected: expected
+    };
+    const message = `Error occurred while testing '${unit}' ('${subUnit}'):\nInput: ${input}\nOutput: ${output}\nExpected: ${expected}`;
     console.error(message);
 }
 
@@ -26,7 +29,7 @@ function makeUnit(unit) {
     results.errorMessages[unit] = {}; 
 }
 
-function typeTest() {
+function typeTests() {
     /*
         Tests for the simple functions of type 
         conversions and type analysis.
@@ -68,14 +71,79 @@ function typeTest() {
             true
         );
     }
+}
 
+function objTests() {
+    const unit = "object-tests";
+    
+    // initialize object
+    const obj = new Mutar("Hello ");
+
+    // ------------------------------------------------ //
+    // testA - conset (tests concat either)
+
+    makeUnit(unit);
+    results.tests++;
+    results.units[unit].tests++;
+    
+    const arrayB = new Uint8Array([87, 111, 114, 108, 100, 33]);
+
+    obj.conset(arrayB);
+
+    // Decode the array
+    const outputA = new TextDecoder().decode(obj.array);
+    const expectedA = "Hello World!";
+
+    if (outputA !== expectedA) {
+        makeError(
+            unit,
+            "conset",
+            arrayB,
+            outputA,
+            expectedA
+        );
+    }
+
+    // ------------------------------------------------ //
+    // testB - convert
+    
+    // convert the array to Uint32 and join to string
+    
+    const inputB = `Uint8Array(${obj.array.join()})`;
+    
+    obj.convert(Uint32Array);
+    
+    // to avoid problems with endianness all uint32 integers are added up
+    // (which can be risky with bigger arrays -> integer overflow)
+    const outputB = obj.array[0] + obj.array[1] + obj.array[2];
+    const expectedB = 4247253545;
+
+    if (outputB !== expectedB) {
+        makeError(
+            unit,
+            "convert",
+            inputB,
+            "Addition of 3 Uint32 -> ".concat(outputB),
+            "Addition of 3 Uint32 -> ".concat(expectedB)
+        );
+    }
+
+    // ------------------------------------------------ //
+    // testB - convert more up and back
+
+    obj.convert(BigUint64Array);
+    console.log(obj.array);
+    console.log(obj.view.getBigUint64(0, true))
+    // 8022916924116329800n
+    obj.convert(Uint8Array);
+    console.log(obj.array);
 
 }
 
 function main() {
-    typeTest();
-
-    console.log(results);
+    typeTests();
+    objTests();
+    console.log(JSON.stringify(results, null, 4));
 }
 
 main();
