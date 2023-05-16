6831df1a8717b2fdca12b0fbdd7ed8e1a4a1e3b4
UmamiAppearance <mail@umamiappearance.eu>
2023-05-16 21:50:08 +0200

    dependency updates / added remembrance dev tool

--- dist/Mutar.cjs
+++ dist/Mutar.cjs
@@ -3,7 +3,7 @@
 /**
  * [Mutar]{@link https://github.com/UmamiAppearance/MutableTypedArrayJS}
  *
- * @version 0.1.6
+ * @version 0.1.7
  * @author UmamiAppearance [mail@umamiappearance.eu]
  * @license GPL-3.0
  *
